4038297cd2a929f3d599e39ff5674e293c8b53ad
Daniel Albuquerque <daniel.alb.querque@gmail.com>
2021-12-20 10:31:00 -0300

    initial

---
+++ Init_WinRM.ps1
@@ -0,0 +1,10 @@
+﻿
+Enable-PSRemoting -SkipNetworkProfileCheck -Force
+Set-Item wsman:\localhost\client\trustedhosts *
+
+cd wsman:localhost\Client
+
+Set-Item AllowUnencrypted -Value $true -force
+Set-Item TrustedHosts -Value * -force
+
+Restart-Service WinRM
---
+++ RestartService_Demo.ps1
@@ -0,0 +1,12 @@
+﻿$Username = 'username@domain'
+$Password = 'supersecretpassword'
+$Server = 'remoteserver'
+
+$pass = ConvertTo-SecureString -AsPlainText $Password -Force
+$Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$pass
+
+Invoke-Command -ComputerName $Server -ScriptBlock { taskkill /F /FI "SERVICES eq .TOTVS_PROTHEUS12_HOMOLOG__3045" } -Credential $Cred
+Invoke-Command -ComputerName $Server -ScriptBlock { taskkill /F /FI "SERVICES eq .TOTVS_PROTHEUS12_HOMOLOG_" } -Credential $Cred
+
+Invoke-Command -ComputerName $Server -ScriptBlock { Net Start .TOTVS_PROTHEUS12_HOMOLOG__3045 } -Credential $Cred
+Invoke-Command -ComputerName $Server -ScriptBlock { Net Start .TOTVS_PROTHEUS12_HOMOLOG__3046 } -Credential $Cred
